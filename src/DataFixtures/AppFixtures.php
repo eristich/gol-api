3f07c29e284208817d264c76e5daccfe99e2f78e
Esteban Ristich <esteban.ristich@protonmail.com>
2024-03-05 21:46:01 +0100

    feat(fixture): create default fixture to generate one user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $hasher
+    ){}
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('esteban');
+        $user->setPassword($this->hasher->hashPassword($user, 'Test#123'));
+        $user->setRoles(['ROLE_USER']);
+        $user->setUpdatedAt(new \DateTimeImmutable());
+        $user->setCreatedAt(new \DateTimeImmutable());
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
